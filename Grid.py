bebbacf280a6fa7c8e64daeeab3eb88355add4bf
Totallynotfair <farise@students.dsbn.org>
2025-11-08 15:36:43 -0500

    fixes

--- Grid.py
+++ Grid.py
@@ -1,14 +1,18 @@
 import pygame
 import sys
 import random
+import math
 
 pygame.init()
-tileSize = 50
-tilesX = 20
-tilesY = 20
+tileSize = 25
+tilesX = 30
+tilesY = 30
+x = 0
+y = 0
 screenX = tileSize * tilesX
 screenY = tileSize * tilesY
 screen = pygame.display.set_mode((screenX, screenY))
+mouseDown = False
 placeHolder = 0
 placeHolder2 = 0
 emptyList = []
@@ -20,7 +24,7 @@ def color(colorType):
         "blank": 'white',
         "metal": 'black',
         "rock": "darkgrey",
-        "water": "blue"
+        "water": "water"
     }
     # I used chatgpt to help me, turns out I was returning the entire dictionary
     return colors.get(colorType, 'red')
@@ -30,18 +34,18 @@ def getAMat():
     if material == 1:
         return "sand"
     elif material == 2:
-        return "blank"
+        return "water"
     elif material == 3:
         return "metal"
     elif material == 4:
         return "rock"
-    elif material == 5:
-        return "water"
+    else:
+        return "blank"
 
 for i2 in range(tilesY):
     rowList = []
     for i in range(tilesX):
-        rowList.append(getAMat())
+        rowList.append("blank")
     twoDList.append(rowList)
 
 while True:
@@ -50,13 +54,26 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    if event.type == pygame.MOUSEMOTION:
+        x, y = pygame.mouse.get_pos()
+
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        mouseDown = True
     
+    if mouseDown:
+        if x > 0 and x < screenX and y > 0 and y < screenY:
+            twoDList[math.floor(y/tileSize)].pop(math.floor(x/tileSize))
+            twoDList[math.floor(y/tileSize)].insert(math.floor(x/tileSize), "metal")        
+
+    if event.type == pygame.MOUSEBUTTONUP:
+        mouseDown = False
+
     screen.fill('lightblue')
     for placeHolder2 in range(0, screenY // tileSize):
         for placeHolder in range(0, screenX // tileSize):
             # Inner
             pygame.draw.rect(screen, color(twoDList[placeHolder2][placeHolder]), (placeHolder * tileSize, placeHolder2 * tileSize, tileSize, tileSize))
             # Boarder
-            pygame.draw.rect(screen, "black", (placeHolder * tileSize, placeHolder2 * tileSize, tileSize, tileSize), 5)
+            pygame.draw.rect(screen, "black", (placeHolder * tileSize, placeHolder2 * tileSize, tileSize, tileSize), tileSize//10)
 
     pygame.display.update()
