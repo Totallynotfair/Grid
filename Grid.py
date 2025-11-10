18d7983c0841e5debb7d1bfdd2a1fcddaaf99774
Totallynotfair <farise@students.dsbn.org>
2025-11-09 22:16:59 -0500

    Physics added

--- Grid.py
+++ Grid.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import random
 import math
+import time
 
 pygame.init()
 tileSize = 25
@@ -17,6 +18,7 @@ checkingX = 0
 checkingY = 0
 drawingX = 0
 drawingY = 0
+borderThickness = 1
 emptyList = []
 twoDList = []
 
@@ -47,39 +49,45 @@ def getAMat():
 def blockCheck(Y, X):
     if twoDList[Y][X] == "sand":
         if twoDList[Y+1][X] == "blank":
-            twoDList[Y+1].pop(X)
-            twoDList[Y+1].insert(X, twoDList[Y][X])
+            twoDList[Y+1][X] = twoDList[Y][X]
+            twoDList[Y][X] = "blank"
         elif twoDList[Y+1][X-1] == "blank":
-            twoDList[Y+1].pop(X-1)
-            twoDList[Y+1].insert(X-1, twoDList[Y][X])
+            twoDList[Y+1][X-1] = twoDList[Y][X]
+            twoDList[Y][X] = "blank"
         elif twoDList[Y+1][X+1] == "blank":
-            twoDList[Y+1].pop(X+1)
-            twoDList[Y+1].insert(X+1, twoDList[Y][X])
+            twoDList[Y+1][X+1] = twoDList[Y][X]
+            twoDList[Y][X] = "blank"
     elif twoDList[Y][X] == "rock":
         if twoDList[Y+1][X] == "blank":
-            twoDList[Y+1].pop(X)
-            twoDList[Y+1].insert(X, twoDList[Y][X])
+            twoDList[Y+1][X] = twoDList[Y][X]
+            twoDList[Y][X] = "blank"
     elif twoDList[Y][X] == "water":
         if twoDList[Y+1][X] == "blank":
-            twoDList[Y+1].pop(X)
-            twoDList[Y+1].insert(X, twoDList[Y][X])
+            twoDList[Y+1][X] = twoDList[Y][X]
+            twoDList[Y][X] = "blank"
         elif twoDList[Y+1][X-1] == "blank":
-            twoDList[Y+1].pop(X-1)
-            twoDList[Y+1].insert(X-1, twoDList[Y][X])
+            twoDList[Y+1][X-1] = twoDList[Y][X]
+            twoDList[Y][X] = "blank"
         elif twoDList[Y+1][X+1] == "blank":
-            twoDList[Y+1].pop(X+1)
-            twoDList[Y+1].insert(X+1, twoDList[Y][X])
+            twoDList[Y+1][X+1] = twoDList[Y][X]
+            twoDList[Y][X] = "blank"
+            # Asked chatgpt to help with this, because I forgot that if I pop a unit in a list, the rest of the list moves down.
         elif twoDList[Y][X-1] == "blank":
-            twoDList[Y].pop(X-1)
-            twoDList[Y].insert(X-1, twoDList[Y][X])
+            twoDList[Y][X-1] = twoDList[Y][X]
+            twoDList[Y][X] = "blank"
         elif twoDList[Y][X+1] == "blank":
-            twoDList[Y].pop(X+1)
-            twoDList[Y].insert(X+1, twoDList[Y][X])
+            twoDList[Y][X+1] = twoDList[Y][X]
+            twoDList[Y][X] = "blank"
 
-for i2 in range(tilesY):
+for i2 in range(tilesY + (borderThickness * 2)):
     rowList = []
-    for i in range(tilesX):
-        rowList.append("blank")
+    for i in range(tilesX + (borderThickness * 2)):
+        if i2 == 0 or i2 == tilesY + (borderThickness):
+            rowList.append("metal")
+        elif i == 0 or i == tilesX + (borderThickness):
+            rowList.append("metal")
+        else:
+            rowList.append("blank")
     twoDList.append(rowList)
 
 while True:
@@ -96,24 +104,26 @@ while True:
 
     if event.type == pygame.MOUSEMOTION:
         x, y = pygame.mouse.get_pos()
-
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        mouseDown = True
     
     if mouseDown:
         if x > 0 and x < screenX and y > 0 and y < screenY:
-            twoDList[math.floor(y/tileSize)].pop(math.floor(x/tileSize))
-            twoDList[math.floor(y/tileSize)].insert(math.floor(x/tileSize), "sand")        
+            twoDList[math.floor(y/tileSize + 1)].pop(math.floor(x/tileSize + 1))
+            twoDList[math.floor(y/tileSize + 1)].insert(math.floor(x/tileSize + 1), "water")        
 
-    if event.type == pygame.MOUSEBUTTONUP:
+    buttons = pygame.mouse.get_pressed()
+    if not any(buttons):
         mouseDown = False
+    else:
+        mouseDown = True
 
     screen.fill('lightblue')
     for drawingY in range(0, screenY // tileSize):
         for drawingX in range(0, screenX // tileSize):
             # Inner
-            pygame.draw.rect(screen, color(twoDList[drawingY][drawingX]), (drawingX * tileSize, drawingY * tileSize, tileSize, tileSize))
+            pygame.draw.rect(screen, color(twoDList[drawingY+borderThickness][drawingX+borderThickness]), (drawingX * tileSize, drawingY * tileSize, tileSize, tileSize))
             # Boarder
             pygame.draw.rect(screen, "black", (drawingX * tileSize, drawingY * tileSize, tileSize, tileSize), tileSize // 10)
+    
+    time.sleep(0.10)
 
     pygame.display.update()
